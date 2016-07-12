6065ebfadbbd215a866f0f7eef48432b89f70400
l123456789jy <906514731@qq.com>
2016-07-12 20:11:50 +0800

    增加条状我的界面，并且可以返回

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -6,12 +6,17 @@ import {
   Image,
   Dimensions,
   WebView,
+  ToolbarAndroid,
 } from 'react-native';
 
 
 var WINDOW_WIDTH = Dimensions.get('window').width;
 var Animated = require('Animated');
 var aboutMeUri="https://github.com/l123456789jy";
+
+//引入返回图标
+var back_bg = require('../img/back.png');
+
 //关于我的主页
 class AboutMe extends Component {
 
@@ -24,12 +29,27 @@ class AboutMe extends Component {
   renderAboutMeView(){
     return (
       <View style={styles.container}>
+
+        <ToolbarAndroid   //标题栏
+          navIcon={back_bg}
+          onIconClicked={this.onBack.bind(this)} //左上角打开侧划栏点击方法
+          titleColor='#ffffff'  //只支持RGB数值，设置标题的字体颜色
+          style={styles.toolbar}
+          title="关于我"></ToolbarAndroid>
+
+
         <WebView
           source={{uri: aboutMeUri}}
-          style={{marginTop: 20}}
         />
       </View>
     );
+  }
+
+
+  //返回
+  onBack(){
+    //将当前界面出栈，就出现后退的效果了
+      this.props.navigator.pop();
   }
 
 }
@@ -42,6 +62,12 @@ const styles = StyleSheet.create({
     flex: 1,  //他自己就占了一份所以铺满全屏
     width: WINDOW_WIDTH,  //宽度设置为屏幕的宽度
     height: 1,
+  },
+
+  toolbar: {
+    backgroundColor: 'blue',
+    height: 56,
+
   },
 });
 
